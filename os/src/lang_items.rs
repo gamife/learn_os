6be79b70e3b149cc5c85ced4fc87a269d5133649
gamife <gamife9886@gmail.com>
2022-12-20 09:45:40 +0000

    draft: init

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,19 +1,6 @@
-//! The panic handler
-
-use crate::sbi::shutdown;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    if let Some(location) = info.location() {
-        println!(
-            "[kernel] Panicked at {}:{} {}",
-            location.file(),
-            location.line(),
-            info.message().unwrap()
-        );
-    } else {
-        println!("[kernel] Panicked: {}", info.message().unwrap());
-    }
-    shutdown()
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
