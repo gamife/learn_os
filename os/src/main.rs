dad42a4f9061564bd0efa4c46146cc4a548521e7
gamife <gamife9886@gmail.com>
2022-12-21 03:52:39 +0000

    feat: init stack

--- os/src/main.rs
+++ os/src/main.rs
@@ -8,7 +8,7 @@
 //! We then call [`println!`] to display `Hello, world!`.
 
 #![deny(missing_docs)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_std]
 #![no_main]
 // #![feature(panic_info_message)]
@@ -16,4 +16,28 @@
 
 mod lang_items;
 
+// 初始化栈
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
 
+/// 程序入口
+#[no_mangle]
+pub fn rust_main() -> !{
+    loop{}
+}
+
+/// 将bss段清零
+fn clear_bss(){
+    extern "C"{
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        // TODO: 验证为啥用 *mut u8
+        // 我猜是地址每加1, 就是一个字节, 比如, 
+        // 0x01 8bit
+        // 0x02 8bit
+        // 所以一个字节的内存, 可以当做u8看待.
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
